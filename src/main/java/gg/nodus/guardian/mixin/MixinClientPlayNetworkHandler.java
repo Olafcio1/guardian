17942feaaa90f1e67deef458afa94dac0ab217a7
Olafcio1 <75330538+Olafcio1@users.noreply.github.com>
2026-08-20 00:41:29 +0200

    Update MixinClientPlayNetworkHandler.java

--- src/main/java/gg/nodus/guardian/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/gg/nodus/guardian/mixin/MixinClientPlayNetworkHandler.java
@@ -13,7 +13,7 @@ import net.minecraft.network.chat.PlayerChatMessage;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChatListener.class)
