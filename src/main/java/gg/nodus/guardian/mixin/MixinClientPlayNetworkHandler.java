e640c19c9073dd88ea216579b6c5d591e015642e
Olafcio1 <75330538+Olafcio1@users.noreply.github.com>
2026-08-20 00:39:48 +0200

    Update MixinClientPlayNetworkHandler.java

--- src/main/java/gg/nodus/guardian/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/gg/nodus/guardian/mixin/MixinClientPlayNetworkHandler.java
@@ -1,22 +1,31 @@
 package gg.nodus.guardian.mixin;
 
+import com.mojang.authlib.GameProfile;
+
+import java.time.Instant;
+
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.multiplayer.chat.ChatListener;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MessageSignature;
+import net.minecraft.network.chat.PlayerChatMessage;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ClientPacketListener.class)
+@Mixin(ChatListener.class)
 public class MixinClientPlayNetworkHandler {
 
     /**
      * LMFAO
      */
     // TODO: Verify is the yarn->mojang translation correct
-    @ModifyVariable(method = "markMessageAsProcessed", at = @At("HEAD"), argsOnly = true, index = 2)
-    public boolean acknowledge(MessageSignature message, boolean value) {
-        return message.getSender().equals(Minecraft.getInstance().player.getUUID());
+    @Inject(method = "showMessageToPlayer", at = @At("RETURN"))
+    public void acknowledge(ChatType.Bound bound, PlayerChatMessage message, Component component, GameProfile gameProfile, boolean bl, Instant instant, CallbackInfoReturnable<Boolean> ci) {
+        ci.setReturnValue(message.sender().equals(Minecraft.getInstance().player.getUUID()));
     }
 
 }
